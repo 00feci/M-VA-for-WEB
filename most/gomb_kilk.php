301b068afbb1fe952c97e0dd4c4825a2a08ba6a0
00feci <04feci@gmail.com>
2025-12-30 10:08:08 +0100

    Update gomb_kilk.php

--- most/gomb_kilk.php
+++ most/gomb_kilk.php
@@ -24,6 +24,9 @@ if ($_POST['funkcio'] === 'Szerződés') {
 }elseif ($_POST['funkcio'] === 'Tömeges_de_egyedi_e-mail') {
     header("Location: /Iroda/verziok/" . $_SESSION['verzio'] . "/Tomeges_de_egyedi_e-mail/tomeges_de_egyedi_e-mail.php");
 }
+    }elseif ($_POST['funkcio'] === 'Beállítások') {
+    header("Location: /Iroda/verziok/" . $_SESSION['verzio'] . "/beallitasok.php");
 // ...stb.
 }
+
 ?>
