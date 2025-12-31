0beb8754ba1b55bbfc56d8aa007d3791313207af
00feci <04feci@gmail.com>
2025-12-31 17:07:32 +0100

    Update sztp_feltoltes.php

--- most/Beallitasok/szabadsag_es_tappenz/sztp_feltoltes.php
+++ most/Beallitasok/szabadsag_es_tappenz/sztp_feltoltes.php
@@ -32,15 +32,22 @@ if (!file_exists($cel_mappa)) {
 }
 
 $fajl = $_FILES['sablon'];
-$fajl_nev = basename($fajl['name']);
-$cel_utvonal = $cel_mappa . $fajl_nev;
+// 📂 Relatív útvonal kezelése mappa-feltöltés esetén
+$relativ_utvonal = $_POST['relativ_utvonal'] ?? basename($fajl['name']);
+$cel_utvonal = $cel_mappa . $relativ_utvonal;
+
+// Szükséges alkönyvtárak létrehozása
+$alkonyvtar = dirname($cel_utvonal);
+if (!file_exists($alkonyvtar)) {
+    mkdir($alkonyvtar, 0777, true);
+}
 
 if (move_uploaded_file($fajl['tmp_name'], $cel_utvonal)) {
     echo json_encode([
         'success' => true, 
         'message' => 'Sikeres feltöltés!', 
-        'fajl_neve' => $fajl_nev,
-        'utvonal' => '/Iroda/Dokumentum_tar/Szabadsag_es_tappenz/Sablonok/' . $fajl_nev
+        'fajl_neve' => $relativ_utvonal,
+        'utvonal' => '/Iroda/Dokumentum_tar/Szabadsag_es_tappenz/Sablonok/' . $relativ_utvonal
     ]);
 } else {
     echo json_encode(['success' => false, 'message' => 'Hiba a fájl mentésekor!']);
