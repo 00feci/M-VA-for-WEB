fb3436cfba47239ae54ea2ef4e359e339e4cb056
00feci <04feci@gmail.com>
2026-01-04 11:50:06 +0100

    Create sztp_fajl_torlese.php

---
+++ most/Beallitasok/szabadsag_es_tappenz/sztp_fajl_torlese.php
@@ -0,0 +1,33 @@
+<?php
+header('Content-Type: application/json');
+$data = json_decode(file_get_contents('php://input'), true);
+$relPath = $data['path'] ?? '';
+$baseDir = "../../../Iroda/Dokumentum_tar/Szabadsag_es_tappenz/Sablonok/";
+$target = realpath($baseDir . $relPath);
+
+// Biztonsági ellenőrzés: csak a kijelölt bázismappán belül engedünk törölni
+if (!$target || strpos($target, realpath($baseDir)) !== 0) {
+    echo json_encode(['success' => false, 'message' => 'Érvénytelen vagy tiltott útvonal!']);
+    exit;
+}
+
+function recursiveDelete($dir) {
+    if (is_dir($dir)) {
+        $objects = scandir($dir);
+        foreach ($objects as $object) {
+            if ($object != "." && $object != "..") {
+                recursiveDelete($dir . "/" . $object);
+            }
+        }
+        return rmdir($dir);
+    } else {
+        return unlink($dir);
+    }
+}
+
+if (recursiveDelete($target)) {
+    echo json_encode(['success' => true]);
+} else {
+    echo json_encode(['success' => false, 'message' => 'A törlés nem sikerült!']);
+}
+?>
