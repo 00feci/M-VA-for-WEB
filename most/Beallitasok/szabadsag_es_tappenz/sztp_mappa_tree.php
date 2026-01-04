4667bf5d073c22aa434b206eb085fd7b7b517a5e
00feci <04feci@gmail.com>
2026-01-04 13:10:19 +0100

    Update sztp_mappa_tree.php

--- most/Beallitasok/szabadsag_es_tappenz/sztp_mappa_tree.php
+++ most/Beallitasok/szabadsag_es_tappenz/sztp_mappa_tree.php
@@ -3,6 +3,11 @@ header('Content-Type: application/json');
 // Egységesítjük az útvonalat az upload scripttel (abszolút elérés)
 $baseDir = $_SERVER['DOCUMENT_ROOT'] . "/Iroda/Dokumentum_tar/Szabadsag_es_tappenz/Sablonok/";
 
+// Biztosítjuk a mappa létezését
+if (!is_dir($baseDir)) {
+    mkdir($baseDir, 0777, true);
+}
+
 function getDirTree($dir, $base) {
     $tree = [];
     if (!is_dir($dir)) return $tree;
