b12cfebb0f38b7734d944214a0d3d1f210f6d468
00feci <04feci@gmail.com>
2026-01-14 13:48:54 +0100

    Create sztp_export.js

---
+++ most/Beallitasok/szabadsag_es_tappenz/js/sztp_export.js
@@ -0,0 +1,12 @@
+// xcxxx kód, ezt a blokot cseréld
+async function globalisSzabalyokMentese() {
+    const fajlnev = document.getElementById('sztp_fajlnev_szabaly')?.value;
+    const exportSzabaly = document.getElementById('sztp_export_szabaly')?.value;
+    
+    if (!fajlnev) return alert("Adj meg egy fájlnév szabályt!");
+    
+    // Itt a jövőben egy fetch hívás fogja menteni ezeket a globális beállításokat
+    console.log("Mentendő szabályok:", { fajlnev, exportSzabaly });
+    alert("Generálási és Export szabályok rögzítve!");
+}
+// kod
