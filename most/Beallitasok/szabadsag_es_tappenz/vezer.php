5a077379da234b6a8eddac8c501109e48fe583a6
00feci <04feci@gmail.com>
2026-02-17 16:01:29 +0100

    Update vezer.php

--- most/Beallitasok/szabadsag_es_tappenz/vezer.php
+++ most/Beallitasok/szabadsag_es_tappenz/vezer.php
@@ -1,6 +1,13 @@
 <?php
-// A legrövidebb kód a fájl behívásához:
+// A vissza gomb behívása
 include 'Vissza a modulokhoz.php';
 
-// Ide jönnek majd sorban a többi gombok (Munkanapok, Hivatkozások, stb.)
+// Modulspecifikus JS fájlok behívása (hogy ne a főoldalt terheljék)
 ?>
+<script src="Beallitasok/szabadsag_es_tappenz/js/sztp_nap_tipusok.js?v=<?php echo filemtime(__DIR__ . '/js/sztp_nap_tipusok.js'); ?>"></script>
+<script src="Beallitasok/szabadsag_es_tappenz/js/sztp_fajtak.js?v=<?php echo filemtime(__DIR__ . '/js/sztp_fajtak.js'); ?>"></script>
+<script src="Beallitasok/szabadsag_es_tappenz/js/sztp_generalas.js?v=<?php echo filemtime(__DIR__ . '/js/sztp_generalas.js'); ?>"></script>
+<script src="Beallitasok/szabadsag_es_tappenz/js/sztp_export.js?v=<?php echo filemtime(__DIR__ . '/js/sztp_export.js'); ?>"></script>
+<script src="Beallitasok/szabadsag_es_tappenz/js/sz_tp_modul.js?v=<?php echo filemtime(__DIR__ . '/js/sz_tp_modul.js'); ?>"></script>
+
+<div id="sz-tp-modul-root"></div>
