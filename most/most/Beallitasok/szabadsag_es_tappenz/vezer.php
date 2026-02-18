8cff961ef875baebcea4c5166b06bf1e70f20dbc
00feci <04feci@gmail.com>
2026-02-18 15:58:29 +0100

    Update vezer.php

--- most/most/Beallitasok/szabadsag_es_tappenz/vezer.php
+++ most/most/Beallitasok/szabadsag_es_tappenz/vezer.php
@@ -1,6 +1,6 @@
 <?php
 // 1. A vissza gomb behívása (Abszolút útvonallal, hogy ne tévedjen el)
-include 'Vissza a modulokhoz.php';
+include __DIR__ . '/Vissza a modulokhoz.php';
 // 2. A JS fájlok behívása a megfelelő sorrendben
 // A src marad relatív a beallitasok.php-hoz képest, de a filemtime abszolút!
 ?>
@@ -10,3 +10,4 @@ include 'Vissza a modulokhoz.php';
 <script src="Beallitasok/szabadsag_es_tappenz/js/sztp_export.js?v=<?php echo filemtime(__DIR__ . '/js/sztp_export.js'); ?>"></script>
 <script src="Beallitasok/szabadsag_es_tappenz/js/sz_tp_modul.js?v=<?php echo filemtime(__DIR__ . '/js/sz_tp_modul.js'); ?>"></script>
 <div id="sz-tp-modul-root"></div>
+
