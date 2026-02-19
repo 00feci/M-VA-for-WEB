58c9f300dd94532bafcf7a7c80cab0a669a4ad98
00feci <04feci@gmail.com>
2026-02-19 13:17:26 +0100

    Update munkanapok_vezer.php

--- most/most/Beallitasok/szabadsag_es_tappenz/Munkanapok típusa/munkanapok_vezer.php
+++ most/most/Beallitasok/szabadsag_es_tappenz/Munkanapok típusa/munkanapok_vezer.php
@@ -1,7 +1,7 @@
 <?php
 // 1. Behúzzuk a felületi gombot
 include __DIR__ . '/munkanapok_gomb.php';
-
+include __DIR__ . 'munkanap_tipus_popup/munkanapok_popup_vezer.php';
 // 2. Később ide jönnek a Popup HTML-ek és a JS fájlok include-jai
 ?>
 
