302b1fad8b45ed7e4a9d501f7e0626c5681e5e90
00feci <04feci@gmail.com>
2026-02-19 11:44:21 +0100

    Update vezer.php

--- most/most/Beallitasok/szabadsag_es_tappenz/vezer.php
+++ most/most/Beallitasok/szabadsag_es_tappenz/vezer.php
@@ -1,6 +1,12 @@
 <?php
 // 1. A vissza gomb behívása (Abszolút útvonallal, hogy ne tévedjen el)
 include __DIR__ . '/Vissza a modulokhoz.php';
+// ✨ ÚJ: Behúzzuk a Munkanapok típusa vezérlőjét (ami megjeleníti a gombot)
+include __DIR__ . '/Munkanapok típusa/munkanapok_vezer.php';
+
+
+
+
 // 2. A JS fájlok behívása a megfelelő sorrendben
 // A src marad relatív a beallitasok.php-hoz képest, de a filemtime abszolút!
 ?>
@@ -10,4 +16,3 @@ include __DIR__ . '/Vissza a modulokhoz.php';
 <script src="Beallitasok/szabadsag_es_tappenz/js/sztp_export.js?v=<?php echo filemtime(__DIR__ . '/js/sztp_export.js'); ?>"></script>
 <script src="Beallitasok/szabadsag_es_tappenz/js/sz_tp_modul.js?v=<?php echo filemtime(__DIR__ . '/js/sz_tp_modul.js'); ?>"></script>
 <div id="sz-tp-modul-root"></div>
-
