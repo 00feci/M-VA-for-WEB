a7f217274416af9d1032ce0295a4f6b54119c152
00feci <04feci@gmail.com>
2026-02-20 14:25:24 +0100

    Update munkanapok_popup_vezer.php

--- most/most/Beallitasok/szabadsag_es_tappenz/Munkanapok típusa/munkanap_tipus_popup/munkanapok_popup_vezer.php
+++ most/most/Beallitasok/szabadsag_es_tappenz/Munkanapok típusa/munkanap_tipus_popup/munkanapok_popup_vezer.php
@@ -2,9 +2,10 @@
 // 1. Behúzzuk a felületi gombot
 include __DIR__ . '/munkanapok_popup.php';
 // 2. Később ide jönnek a Popup HTML-ek és a JS fájlok include-jai
-include __DIR__ . '/munkanapok_popup_kod_szin.php'; ?>
-                        </div>
+include __DIR__ . '/munkanapok_popup_kod_szin.php'; 
 
+?>
+ </div>
             <div style="display: flex; flex-direction: column; gap: 10px; margin-top: 20px;">
                <div style="display: flex; gap: 10px;">
                     <?php include __DIR__ . '/munkanapok_popup_mentes.php'; ?>
@@ -15,11 +16,10 @@ include __DIR__ . '/munkanapok_popup_kod_szin.php'; ?>
                    <?php include __DIR__ . '/Sablon kezelése/popup_sablon_kezelese_vezer.php'; ?>
                 </div>
                  <?php include __DIR__ . '/munkanapok_popup_nagy_rekord.php'; ?>
-                    <?php include __DIR__ . '/munkanapok_popup_bezaras.php'; ?>
+                  <?php include __DIR__ . '/munkanapok_popup_bezaras.php'; ?>
                 </div>
             </div>
         </div>
-         <?php include __DIR__ . '/munkanapok_popup_sablon_fajlok_listaja.php'; ?>
+          <?php include __DIR__ . '/munkanapok_popup_sablon_fajlok_listaja.php'; ?>
     </div>
 </div>
-?>
