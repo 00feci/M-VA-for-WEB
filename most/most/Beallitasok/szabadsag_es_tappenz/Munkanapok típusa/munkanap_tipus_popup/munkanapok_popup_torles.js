9ce96a966a35d3092de835e23b09a904e1a1b7a9
00feci <04feci@gmail.com>
2026-02-20 15:10:25 +0100

    Create munkanapok_popup_torles.js

---
+++ most/most/Beallitasok/szabadsag_es_tappenz/Munkanapok típusa/munkanap_tipus_popup/munkanapok_popup_torles.js
@@ -0,0 +1,19 @@
+function beallitasokTorlese() {
+    const id = document.getElementById('sztp_id').value;
+    if (!id) return alert("Nincs kiválasztva mentett beállítás!");
+    if (confirm("Biztosan törölni szeretnéd ezt a beállítást?")) {
+        fetch('Beallitasok/szabadsag_es_tappenz/sztp_torlese.php', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({ id: id })
+        })
+        .then(r => r.json())
+        .then(data => {
+            alert(data.message);
+            if (data.success) {
+                adatokBetoltese(''); 
+                listaBetoltese();   
+            }
+        });
+    }
+}
