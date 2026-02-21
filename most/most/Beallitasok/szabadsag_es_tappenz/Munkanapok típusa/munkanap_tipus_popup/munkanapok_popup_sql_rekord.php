cf70ced02a6515523262b1eb37018b1c1c8e0f99
00feci <04feci@gmail.com>
2026-02-21 07:23:57 +0100

    Update munkanapok_popup_sql_rekord.php

--- most/most/Beallitasok/szabadsag_es_tappenz/Munkanapok típusa/munkanap_tipus_popup/munkanapok_popup_sql_rekord.php
+++ most/most/Beallitasok/szabadsag_es_tappenz/Munkanapok típusa/munkanap_tipus_popup/munkanapok_popup_sql_rekord.php
@@ -1,8 +1,9 @@
 <div style="display: flex; gap: 10px;">
                     <div style="flex: 1; display: flex; align-items: center; justify-content: space-between; background: #252525; padding: 8px 12px; border-radius: 8px; border: 1px solid #444;">
-                        <label style="font-size: 0.75em; color: #aaa; font-weight: bold;">NAGY rekord?</label>
+                        <label style="font-size: 0.75em; color: #aaa; font-weight: bold;">Dokumentum?</label>
                         <select id="sztp_nagy_rekord" style="padding: 4px; background: #121212; border: 1px solid #333; color: white; border-radius: 4px; cursor: pointer; font-size: 0.85em;">
                             <option value="nem">Nem</option>
                             <option value="igen">Igen</option>
                         </select>
                     </div>
+
