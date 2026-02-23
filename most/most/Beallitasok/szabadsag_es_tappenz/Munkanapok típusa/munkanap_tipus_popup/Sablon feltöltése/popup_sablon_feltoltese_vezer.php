49bf60b7b596bbd26e18022fa05a86e5b6c0f796
00feci <04feci@gmail.com>
2026-02-23 15:31:20 +0100

    Update popup_sablon_feltoltese_vezer.php

--- most/most/Beallitasok/szabadsag_es_tappenz/Munkanapok típusa/munkanap_tipus_popup/Sablon feltöltése/popup_sablon_feltoltese_vezer.php
+++ most/most/Beallitasok/szabadsag_es_tappenz/Munkanapok típusa/munkanap_tipus_popup/Sablon feltöltése/popup_sablon_feltoltese_vezer.php
@@ -1,8 +1,9 @@
 <?php
 // 1. A gomb HTML-je
-include __DIR__ . '/popup_sablon_feltoltese_gomb.php';
+include __DIR__ . '/popup_sablon_feltoltese_gomb.html';
 
 // 2. A gomb saját JavaScriptje
 $scriptFeltoltGomb = 'Beallitasok/szabadsag_es_tappenz/Munkanapok típusa/munkanap_tipus_popup/Sablon feltöltése/popup_sablon_feltoltese_gomb.js';
 echo "<script src='{$scriptFeltoltGomb}?v=" . time() . "'></script>";
 ?>
+
