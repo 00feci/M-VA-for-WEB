0d111703323b0ca429b56a6ba89b1718dc08ae53
00feci <04feci@gmail.com>
2026-02-23 15:25:21 +0100

    Update munkanapok_popup_vezer.php

--- most/most/Beallitasok/szabadsag_es_tappenz/Munkanapok típusa/munkanap_tipus_popup/munkanapok_popup_vezer.php
+++ most/most/Beallitasok/szabadsag_es_tappenz/Munkanapok típusa/munkanap_tipus_popup/munkanapok_popup_vezer.php
@@ -14,7 +14,7 @@ ellenorizJogosultsag('Beállítások'); // Csak ezt a szót kell átírni!
 // 1. Behúzzuk a felületi gombot
 include __DIR__ . '/munkanapok_popup.php';
 // 2. Később ide jönnek a Popup HTML-ek és a JS fájlok include-jai
-include __DIR__ . '/munkanapok_popup_kod_szin.php'; 
+include __DIR__ . '/munkanapok_popup_kod_szin.html'; 
 
 
 // JS Fájlok betöltése
@@ -42,23 +42,24 @@ echo "<script src='{$scriptFajlok}?v=" . time() . "'></script>";
 $scriptAdat = 'Beallitasok/szabadsag_es_tappenz/Munkanapok típusa/munkanap_tipus_popup/munkanapok_popup_sql_rekord.js';
 echo "<script src='{$scriptAdat}?v=" . time() . "'></script>";
 
+
 // popup váz
 ?> 
  </div>
             <div style="display: flex; flex-direction: column; gap: 10px; margin-top: 20px;">
                <div style="display: flex; gap: 10px;">
-                    <?php include __DIR__ . '/munkanapok_popup_mentes.php'; ?>
+                    <?php include __DIR__ . '/munkanapok_popup_mentes.html'; ?>
                     <?php include __DIR__ . '/Sablon feltöltése/popup_sablon_feltoltese_vezer.php'; ?>
                 </div>
                <div style="display: flex; gap: 10px;">
-                    <?php include __DIR__ . '/munkanapok_popup_torles.php'; ?>
+                    <?php include __DIR__ . '/munkanapok_popup_torles.html'; ?>
                    <?php include __DIR__ . '/Sablon kezelése/popup_sablon_kezelese_vezer.php'; ?>
                 </div>
-                 <?php include __DIR__ . '/munkanapok_popup_sql_rekord.php'; ?>
-                  <?php include __DIR__ . '/munkanapok_popup_bezaras.php'; ?>
+                 <?php include __DIR__ . '/munkanapok_popup_sql_rekord.html'; ?>
+                  <?php include __DIR__ . '/munkanapok_popup_bezaras.html'; ?>
                 </div>
             </div>
         </div>
-          <?php include __DIR__ . '/munkanapok_popup_sablon_fajlok_listaja.php'; ?>
+          <?php include __DIR__ . '/munkanapok_popup_sablon_fajlok_listaja.html'; ?>
     </div>
 </div>
