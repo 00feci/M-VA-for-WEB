91fb0d7a9511a1305ba9d85db3c9500340924f74
00feci <04feci@gmail.com>
2026-02-23 15:33:30 +0100

    Update popup_sablon_kezelese_vezer.php

--- most/most/Beallitasok/szabadsag_es_tappenz/Munkanapok típusa/munkanap_tipus_popup/Sablon kezelése/popup_sablon_kezelese_vezer.php
+++ most/most/Beallitasok/szabadsag_es_tappenz/Munkanapok típusa/munkanap_tipus_popup/Sablon kezelése/popup_sablon_kezelese_vezer.php
@@ -1,8 +1,9 @@
 <?php
 // 1. A gomb HTML-je
-include __DIR__ . '/popup_sablon_kezelese_gomb.php';
+include __DIR__ . '/popup_sablon_kezelese_gomb.html';
 
 // 2. A gomb saját JavaScriptje
 $scriptKezelGomb = 'Beallitasok/szabadsag_es_tappenz/Munkanapok típusa/munkanap_tipus_popup/Sablon kezelése/popup_sablon_kezelese_gomb.js';
 echo "<script src='{$scriptKezelGomb}?v=" . time() . "'></script>";
 ?>
+
