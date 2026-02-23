161e6d308bb4b178140a2e1d14aac834ed93f28b
00feci <04feci@gmail.com>
2026-02-23 15:41:23 +0100

    Update popup_munkanap_tipusa_vezer.php

--- most/most/Beallitasok/szabadsag_es_tappenz/Munkanapok típusa/munkanap_tipus_popup/Munkanap tipusa/popup_munkanap_tipusa_vezer.php
+++ most/most/Beallitasok/szabadsag_es_tappenz/Munkanapok típusa/munkanap_tipus_popup/Munkanap tipusa/popup_munkanap_tipusa_vezer.php
@@ -1,2 +1,16 @@
- <?php include __DIR__ . '/popup_munkanap_tipusa.php'; ?>
- <?php include __DIR__ . '/popup_munkanap_tipusa_plusz_popup.php'; ?>
+ <?php
+ // --- JOGOSULTSÁG ELLENŐRZÉSE ---
+require_once $_SERVER['DOCUMENT_ROOT'] . '/wp-load.php';
+// 1. Biztosítjuk, hogy a Session fusson, mielőtt kiolvassuk a verziót
+if (session_status() === PHP_SESSION_NONE) { session_start(); }
+
+// 2. Összerakjuk a dinamikus, "telepítési" útvonalat
+$verzio = $_SESSION['verzio'] ?? ''; // Ha valamiért üres lenne, ne dőljön össze
+require_once $_SERVER['DOCUMENT_ROOT'] . '/Iroda/eles_verziok/' . $verzio . '/jogosultsag.php';
+// 3. Ellenőrzés
+ellenorizJogosultsag('Beállítások'); // Csak ezt a szót kell átírni!
+
+  include __DIR__ . '/popup_munkanap_tipusa.html'; 
+ include __DIR__ . '/popup_munkanap_tipusa_plusz_popup.html';
+ 
+ ?>
