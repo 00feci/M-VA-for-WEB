5acbed6274de7417f69ce704cda6bd880a33a6c1
00feci <04feci@gmail.com>
2026-02-24 09:25:10 +0100

    Update sztp_mentes.php

--- most/most/Beallitasok/szabadsag_es_tappenz/sztp_mentes.php
+++ most/most/Beallitasok/szabadsag_es_tappenz/sztp_mentes.php
@@ -1,14 +1,9 @@
 <?php
 // --- JOGOSULTSÁG ELLENŐRZÉSE ---
 require_once $_SERVER['DOCUMENT_ROOT'] . '/wp-load.php';
-// 1. Biztosítjuk, hogy a Session fusson, mielőtt kiolvassuk a verziót
-if (session_status() === PHP_SESSION_NONE) { session_start(); }
-
-// 2. Összerakjuk a dinamikus, "telepítési" útvonalat
-$verzio = $_SESSION['verzio'] ?? ''; // Ha valamiért üres lenne, ne dőljön össze
-require_once $_SERVER['DOCUMENT_ROOT'] . '/Iroda/eles_verziok/' . $verzio . '/jogosultsag.php';
-// 3. Ellenőrzés
-ellenorizJogosultsag('Beállítások'); // Csak ezt a szót kell átírni!
+require_once __DIR__.'/../../jogosultsag.php';
+ellenorizJogosultsag('Beállítások');
+// -------------------------------
 
 $pdo = csatlakozasSzerver1();
 
@@ -74,4 +69,3 @@ try {
 } catch (Exception $e) {
     echo json_encode(['success' => false, 'message' => 'Adatbázis hiba: ' . $e->getMessage()]);
 }
-
