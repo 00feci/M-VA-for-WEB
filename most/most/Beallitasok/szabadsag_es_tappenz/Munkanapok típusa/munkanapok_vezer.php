3d040e343d54ec53efdfd50c89fff37ce025af93
00feci <04feci@gmail.com>
2026-02-24 09:39:13 +0100

    Update munkanapok_vezer.php

--- most/most/Beallitasok/szabadsag_es_tappenz/Munkanapok típusa/munkanapok_vezer.php
+++ most/most/Beallitasok/szabadsag_es_tappenz/Munkanapok típusa/munkanapok_vezer.php
@@ -1,17 +1,12 @@
 <?php
 // --- JOGOSULTSÁG ELLENŐRZÉSE ---
 require_once $_SERVER['DOCUMENT_ROOT'] . '/wp-load.php';
-// 1. Biztosítjuk, hogy a Session fusson, mielőtt kiolvassuk a verziót
-if (session_status() === PHP_SESSION_NONE) { session_start(); }
-
-// 2. Összerakjuk a dinamikus, "telepítési" útvonalat
-$verzio = $_SESSION['verzio'] ?? ''; // Ha valamiért üres lenne, ne dőljön össze
-require_once $_SERVER['DOCUMENT_ROOT'] . '/Iroda/eles_verziok/' . $verzio . '/jogosultsag.php';
-// 3. Ellenőrzés
-ellenorizJogosultsag('Beállítások'); // Csak ezt a szót kell átírni!
+require_once __DIR__.'/../../../jogosultsag.php';
+ellenorizJogosultsag('Beállítások');
+// -------------------------------
 
 // 1. Megjelenítjük a gombot a felületen
-include __DIR__ . '/munkanapok_gomb.php';
+include __DIR__ . '/munkanapok_gomb.html';
 
 // 2. Betöltjük a felugró ablak (popup) HTML-jét a memóriába (rejtve)
 include __DIR__ . '/munkanap_tipus_popup/munkanapok_popup_vezer.php';
