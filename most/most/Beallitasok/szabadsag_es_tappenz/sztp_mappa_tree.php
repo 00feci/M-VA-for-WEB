361e1cf21fd53f0107972e2c96534ce457bffee6
00feci <04feci@gmail.com>
2026-02-24 09:25:48 +0100

    Update sztp_mappa_tree.php

--- most/most/Beallitasok/szabadsag_es_tappenz/sztp_mappa_tree.php
+++ most/most/Beallitasok/szabadsag_es_tappenz/sztp_mappa_tree.php
@@ -1,15 +1,9 @@
 <?php
 // --- JOGOSULTSÁG ELLENŐRZÉSE ---
 require_once $_SERVER['DOCUMENT_ROOT'] . '/wp-load.php';
-// 1. Biztosítjuk, hogy a Session fusson, mielőtt kiolvassuk a verziót
-if (session_status() === PHP_SESSION_NONE) { session_start(); }
-
-// 2. Összerakjuk a dinamikus, "telepítési" útvonalat
-$verzio = $_SESSION['verzio'] ?? ''; // Ha valamiért üres lenne, ne dőljön össze
-require_once $_SERVER['DOCUMENT_ROOT'] . '/Iroda/eles_verziok/' . $verzio . '/jogosultsag.php';
-// 3. Ellenőrzés
-ellenorizJogosultsag('Beállítások'); // Csak ezt a szót kell átírni!
-
+require_once __DIR__.'/../../jogosultsag.php';
+ellenorizJogosultsag('Beállítások');
+// -------------------------------
 
 header('Content-Type: application/json');
 date_default_timezone_set('Europe/Budapest'); // 🕒 Időzóna beállítása a magyar időhöz
