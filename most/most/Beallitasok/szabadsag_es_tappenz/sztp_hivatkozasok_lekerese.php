1c450f527574bbae0d6e2668267fa7888cad9a89
00feci <04feci@gmail.com>
2026-02-24 09:30:28 +0100

    Update sztp_hivatkozasok_lekerese.php

--- most/most/Beallitasok/szabadsag_es_tappenz/sztp_hivatkozasok_lekerese.php
+++ most/most/Beallitasok/szabadsag_es_tappenz/sztp_hivatkozasok_lekerese.php
@@ -1,27 +1,10 @@
 <?php
-<?php
 // --- JOGOSULTSÁG ELLENŐRZÉSE ---
 require_once $_SERVER['DOCUMENT_ROOT'] . '/wp-load.php';
 require_once __DIR__.'/../../jogosultsag.php';
 ellenorizJogosultsag('Beállítások');
 // -------------------------------
 
-header('Content-Type: application/json');
-require_once $_SERVER['DOCUMENT_ROOT'] . '/Iroda/sql_config.php';
-
-try {
-    $pdo = csatlakozasSzerver1();
-    $data = json_decode(file_get_contents('php://input'), true);
-    
-    if (!isset($data['lista'])) throw new Exception("Hiányzó adatok.");
-
-    $stmt = $pdo->prepare("UPDATE szabadsag_es_tappenz_beallitasok SET extra_adatok = ? WHERE megnevezes = 'RENDSZER_HIVATKOZASOK'");
-    $stmt->execute([json_encode($data['lista'], JSON_UNESCAPED_UNICODE)]);
-
-    echo json_encode(['success' => true, 'message' => 'Sorrend frissítve!']);
-} catch (Exception $e) {
-    echo json_encode(['success' => false, 'message' => $e->getMessage()]);
-}
 
 header('Content-Type: application/json');
 require_once $_SERVER['DOCUMENT_ROOT'] . '/Iroda/sql_config.php';
@@ -37,4 +20,3 @@ try {
     echo json_encode(['success' => false, 'lista' => [], 'message' => $e->getMessage()]); 
 }
 ?>
-
