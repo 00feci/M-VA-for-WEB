4f08b1cc27e9132a4a130038fc886ae7aafc82cb
00feci <04feci@gmail.com>
2026-02-24 09:18:22 +0100

    Update vezer.php

--- most/most/Beallitasok/szabadsag_es_tappenz/vezer.php
+++ most/most/Beallitasok/szabadsag_es_tappenz/vezer.php
@@ -1,14 +1,9 @@
 <?php
 // --- JOGOSULTSÁG ELLENŐRZÉSE ---
 require_once $_SERVER['DOCUMENT_ROOT'] . '/wp-load.php';
-// 1. Biztosítjuk, hogy a Session fusson, mielőtt kiolvassuk a verziót
-if (session_status() === PHP_SESSION_NONE) { session_start(); }
-// 2. Összerakjuk a dinamikus, "telepítési" útvonalat
-$verzio = $_SESSION['verzio'] ?? ''; // Ha valamiért üres lenne, ne dőljön össze
-require_once $_SERVER['DOCUMENT_ROOT'] . '/Iroda/eles_verziok/' . $verzio . '/jogosultsag.php';
-// 3. Ellenőrzés
-ellenorizJogosultsag('Beállítások'); // Csak ezt a szót kell átírni!
-
+require_once __DIR__.'/../../jogosultsag.php';
+ellenorizJogosultsag('Beállítások');
+// -------------------------------
 
 // 1. A vissza gomb behívása (Abszolút útvonallal, hogy ne tévedjen el)
 include __DIR__ . '/Vissza a modulokhoz.php';
@@ -27,3 +22,4 @@ include __DIR__ . '/Munkanapok típusa/munkanapok_vezer.php';
 <script src="Beallitasok/szabadsag_es_tappenz/js/sztp_export.js?v=<?php echo filemtime(__DIR__ . '/js/sztp_export.js'); ?>"></script>
 <script src="Beallitasok/szabadsag_es_tappenz/js/sz_tp_modul.js?v=<?php echo filemtime(__DIR__ . '/js/sz_tp_modul.js'); ?>"></script>
 <div id="sz-tp-modul-root"></div>
+
