2f224871b9c6b12f52cb4c3e4f3f8552990bca48
00feci <04feci@gmail.com>
2026-03-19 13:51:06 +0100

    Update napok_vezer.php

--- most/most/Beallitasok/szabadsag_es_tappenz/Napok típusa/napok_vezer.php
+++ most/most/Beallitasok/szabadsag_es_tappenz/Napok típusa/napok_vezer.php
@@ -11,25 +11,5 @@ include __DIR__ . '/napok_típusa.html';
 // 2. Betöltjük a felugró ablak HTML-jét (EZ HIÁNYZOTT!)
 include __DIR__ . '/nap_tipusok_kezelese.html';
 ?>
-<script src="Beallitasok/szabadsag_es_tappenz/Napok%20típusa/nap_tipusok_kezelese.js?v=<?php echo filemtime(__DIR__ . '/nap_tipusok_kezelese.js'); ?>"></script>
-<script>
-// Átneveztük a függvényt, hogy ne akadjon össze a sztp_fajtak.js-ben lévővel
-function napTipusListaBetoltese() {
-    fetch('Beallitasok/szabadsag_es_tappenz/sztp_lekerese.php?v=' + new Date().getTime())
-        .then(r => r.json())
-        .then(data => {
-            if (!data.success || !data.lista) return;
-
-            // Csak a Globális naptípusokat keressük
-            const globalRecord = data.lista.find(i => i.megnevezes === "GLOBAL_NAP_TIPUSOK");
-            if (globalRecord && typeof adatokBetoltese === 'function') {
-                adatokBetoltese(globalRecord.id);
-            }
-        });
-}
-
-// Opcionális: Ha rögtön az oldal betöltésekor be akarod tölteni az adatokat a háttérben:
-document.addEventListener('DOMContentLoaded', function() {
-    napTipusListaBetoltese();
-});
-</script>
+<script src="Beallitasok/szabadsag_es_tappenz/Napok típusa/nap_tipusok_kezelese.js?v=<?php echo filemtime(__DIR__ . '/nap_tipusok_kezelese.js'); ?>"></script>
+<script src="Beallitasok/szabadsag_es_tappenz/Napok típusa/napok_típusa.js?v=<?php echo filemtime(__DIR__ . '/napok_típusa.js'); ?>"></script>
