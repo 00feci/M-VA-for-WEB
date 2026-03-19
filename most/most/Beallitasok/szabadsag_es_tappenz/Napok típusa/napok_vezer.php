61cf768b81782ebf35dbb6a61b56ffd8db83ebff
00feci <04feci@gmail.com>
2026-03-19 06:05:42 +0100

    Update napok_vezer.php

--- most/most/Beallitasok/szabadsag_es_tappenz/Napok típusa/napok_vezer.php
+++ most/most/Beallitasok/szabadsag_es_tappenz/Napok típusa/napok_vezer.php
@@ -10,3 +10,29 @@ include __DIR__ . '/napok_típusa.html';
 include __DIR__ . '/nap_tipusok_kezelese.html';
 ?>
 <script src="Beallitasok/szabadsag_es_tappenz/Napok típusa/nap_tipusok_kezelese.js?v=<?php echo filemtime(__DIR__ . '/nap_tipusok_kezelese.js'); ?>"></script>
+<script>
+// Amint betöltődött a modul, azonnal lekérjük az adatokat
+(function() {
+    // Itt meg kell hívnunk azt a funkciót, ami feltölti a listát.
+    // Mivel a "Napok típusa" a globális listából jön, megkeressük az azonosítóját.
+    fetch('Beallitasok/szabadsag_es_tappenz/sztp_lekerese.php')
+        .then(r => r.json())
+        .then(data => {
+            if (data.success) {
+                // Megkeressük a GLOBAL_NAP_TIPUSOK bejegyzést
+                const napTipusAdat = data.lista.find(i => i.megnevezes === "GLOBAL_NAP_TIPUSOK");
+                if (napTipusAdat && typeof adatokBetoltese === 'function') {
+                    // Betöltjük a konkrét típusokat a rejtett select-be
+                    adatokBetoltese(napTipusAdat.id, true);
+                    
+                    // Kis várakozás után (hogy a fetch lefusson) frissítjük a látható listát
+                    setTimeout(() => {
+                        if (typeof napTipusListaFrissitese === 'function') {
+                            napTipusListaFrissitese();
+                        }
+                    }, 200);
+                }
+            }
+        });
+})();
+</script>
