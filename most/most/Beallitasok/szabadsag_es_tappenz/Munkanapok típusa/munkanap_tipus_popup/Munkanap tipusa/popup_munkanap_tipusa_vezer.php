552dc613e15be2cf90dbea21f493d0c9efbd1f7d
00feci <04feci@gmail.com>
2026-03-24 05:54:32 +0100

    Update popup_munkanap_tipusa_vezer.php

--- most/most/Beallitasok/szabadsag_es_tappenz/Munkanapok típusa/munkanap_tipus_popup/Munkanap tipusa/popup_munkanap_tipusa_vezer.php
+++ most/most/Beallitasok/szabadsag_es_tappenz/Munkanapok típusa/munkanap_tipus_popup/Munkanap tipusa/popup_munkanap_tipusa_vezer.php
@@ -5,8 +5,8 @@ require_once __DIR__.'/../../../../../jogosultsag.php';
 ellenorizJogosultsag('Beállítások');
 // -------------------------------
 
+
   include __DIR__ . '/popup_munkanap_tipusa.html'; 
  include __DIR__ . '/popup_munkanap_tipusa_plusz_popup.html';
- 
  ?>
-
+ <script src="Beallitasok/szabadsag_es_tappenz/Munkanapok típusa/munkanap_tipus_popup/Munkanap tipusa/popup_munkanap_tipusa.js?v=<?php echo filemtime(__DIR__ . '/popup_munkanap_tipusa.js'); ?>"></script>
