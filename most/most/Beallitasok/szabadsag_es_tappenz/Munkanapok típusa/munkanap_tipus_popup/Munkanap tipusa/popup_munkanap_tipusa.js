d7749e421b7f5221da55612d8a1e966491d7d4e5
00feci <04feci@gmail.com>
2026-03-24 06:01:28 +0100

    Update popup_munkanap_tipusa.js

--- most/most/Beallitasok/szabadsag_es_tappenz/Munkanapok típusa/munkanap_tipus_popup/Munkanap tipusa/popup_munkanap_tipusa.js
+++ most/most/Beallitasok/szabadsag_es_tappenz/Munkanapok típusa/munkanap_tipus_popup/Munkanap tipusa/popup_munkanap_tipusa.js
@@ -1,3 +1,17 @@
+function szTpModulBetoltese() {
+    // Szigorúan a vezer.php-ban definiált root div-et keressük, 
+    // hogy ne írjuk felül a felette lévő PHP-s Vissza gombot!
+    const kontener = document.getElementById('sz-tp-modul-root');
+    
+    if (!kontener) {
+        console.warn("Az 'sz-tp-modul-root' nem található, próbálkozás a 'modul-tartalom' elemmel...");
+        const tartalmiKontener = document.getElementById('modul-tartalom');
+        if (!tartalmiKontener) return;
+        // Ha nincs root div, de van tartalmi konténer, létrehozzuk a helyet
+        tartalmiKontener.innerHTML = '<div id="sz-tp-modul-root"></div>';
+        return szTpModulBetoltese();
+    }
+
 kontener.innerHTML = `
 <div id="sztp-feltolto-modal-kontener"></div>
         <div id="sztp-nap-modal-kontener"></div>
@@ -28,3 +42,4 @@ kontener.innerHTML = `
                 }
             }).catch(err => console.error("Hiba a HTML betöltésekor:", err));
     }, 150);
+}
